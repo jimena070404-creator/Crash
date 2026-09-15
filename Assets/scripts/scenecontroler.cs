49bab836a8e205d707cffbc7312433958493ff64
Stunimakey <jimena070404@gmail.com>
2026-09-14 20:29:56 -0600

    NuevaEscena

---
+++ Assets/scripts/scenecontroler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class scenecontroler : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent onSceneStarted;
+    [SerializeField]
+    private Animator fade;
+    [SerializeField]
+    private string fadeOutAnimationName = "FadeOut";
+
+    private void Start()
+    {
+        onSceneStarted?.Invoke();
+    }
+    public void GoToSceneWithFade(string sceneName)
+    {
+        StartCoroutine(LoadSceneWithFade(sceneName));
+    }
+    private IEnumerator LoadSceneWithFade(string sceneName)
+    {
+        fade.Play(fadeOutAnimationName, 0, 0f);
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
